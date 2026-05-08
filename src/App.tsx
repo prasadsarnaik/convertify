d1ad7100af3419f1501f5505707059b1c31cee38
prasadsarnaik <prasadsarnaik028@gmail.com>
2026-05-08 13:25:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
@@ -14,12 +14,37 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <AppRoutes />
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
 );
 
 export default App;
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { BrowserRouter } from "react-router-dom";
+// import { HelmetProvider } from "react-helmet-async";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { Toaster } from "@/components/ui/toaster";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import AppRoutes from "@/AppRoutes";
+
+// const queryClient = new QueryClient();
+
+// const App = () => (
+//   <HelmetProvider>
+//     <QueryClientProvider client={queryClient}>
+//       <TooltipProvider>
+//         <Toaster />
+//         <Sonner />
+//         <BrowserRouter>
+//           <AppRoutes />
+//         </BrowserRouter>
+//       </TooltipProvider>
+//     </QueryClientProvider>
+//   </HelmetProvider>
+// );
+
+// export default App;
