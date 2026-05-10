e47a67b72e1abe2a4aa320265d68b453e8fb61cc
Animesh <animeshbhople885507@gmail.com>
2026-05-10 17:37:05 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import compression from "vite-plugin-compression";
+// @ts-ignore
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,6 +16,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    mode === "development" && componentTagger(),
     mode === "production" &&
       compression({
         algorithm: "gzip",
